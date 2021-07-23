d42adc0a4aae70a94790692c6224355c5f3333c8
Muhammad Yahiya <64965102+Yahiya166@users.noreply.github.com>
2021-07-23 17:51:54 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,166 @@
+// get element by ID
+
+let email = document.getElementById("email");
+let password = document.getElementById("password");
+
+
+
+
+
+// function of gotoSignUp()
+
+function gotoSignUp() {
+
+    window.location.replace("index1.html");
+}
+
+
+
+
+
+// function of gotoSignUp()
+
+function gotoLogin() {
+
+    window.location.replace("index.html");
+}
+
+
+
+
+// function  of  SignUp()
+
+function SignUp() {
+    firebase.auth().createUserWithEmailAndPassword(email.value, password.value).then((result) => {
+
+        alert("user signup successfully")
+
+        window.location.replace("index.html")
+        // console.log(result)
+
+    })
+
+
+
+        .catch(function (error) {
+            // Handle Errors here.
+            // var errorCode = error.code;
+            var errorMessage = error.message;
+
+
+            alert(errorMessage)
+        });
+}
+
+
+
+// function  of  Login()
+
+
+function Login() {
+    firebase.auth().signInWithEmailAndPassword(email.value, password.value).then((result) => {
+        alert("user login successfully")
+        // var locate = window.location.href
+        // console.log(locate)
+
+        window.location.replace("index2.html")
+        // console.log(result)
+    }).catch((error) => {
+        var errorMessage = error.message;
+
+        alert(errorMessage);
+    })
+}
+
+
+
+// function  of  GoogleLogin()
+
+function GoogleLogin() {
+    var provider = new firebase.auth.GoogleAuthProvider();
+
+    firebase.auth()
+        .signInWithPopup(provider)
+        .then((result) => {
+            /** @type {firebase.auth.OAuthCredential} */
+            var credential = result.credential;
+
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            var token = credential.accessToken;
+            // The signed-in user info.
+            var user = result.user;
+            window.location.replace("index2.html");
+
+            // var hello = document.getElementById("hello").innerHTML = "Wellcome" + user.displayName + "to our WebSite";
+            // hello = "Wellcome" + user.displayName + "to our WebSite";
+
+            // console.log(user);
+            // console.log(token);
+            // alert(user.displayName + " is login successfully");
+
+        }).catch((error) => {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // The email of the user's account used.
+            var email = error.email;
+            // The firebase.auth.AuthCredential type that was used.
+            var credential = error.credential;
+            console.log(errorMessage);
+            console.log(email);
+        });
+
+
+}
+
+// function  of  FacebookLogin()
+
+
+function facebookLogin() {
+    var provider = new firebase.auth.FacebookAuthProvider();
+
+    firebase
+        .auth()
+        .signInWithPopup(provider)
+        .then((result) => {
+            /** @type {firebase.auth.OAuthCredential} */
+            var credential = result.credential;
+
+            // The signed-in user info.
+            var user = result.user;
+
+            // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+            var accessToken = credential.accessToken;
+
+            // window.location.replace("index2.html")
+
+            console.log(user);
+
+        })
+        .catch((error) => {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // The email of the user's account used.
+            var email = error.email;
+            // The firebase.auth.AuthCredential type that was used.
+            var credential = error.credential;
+
+            alert(errorMessage);
+
+        });
+}
+
+
+
+
+// function  of  Logout()
+
+function Logout() {
+    firebase.auth().signOut().then(() => {
+        alert("user logout successful")
+        window.location.replace("index.html")
+    }).catch((error) => {
+        console.log(error.message);
+    });
+}
